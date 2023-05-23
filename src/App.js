be5875f806004735db48227c8fb3a3a67d1755fc
YaaIlya <118045936+YaaIlya@users.noreply.github.com>
2023-05-23 19:57:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className='app-wraper'>
-      <header className='header'>
-
-      </header >
-
-      <div className='content'>
-        <div className="heart">
-          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png' />
-          <p className='pp'>
-            GameHarmony
-          </p>
-          <p className='ppp'>
-            Finally, the answer to "What will we play together in the evening?"
-          </p>
-        </div>
-        <div className="windows">
-          <div className="window">
-            <input type="text" placeholder="Pick your Game" />
-          </div>
-          <div className="plus">
-            +
-          </div>
-          <div className="window">
-            <input type="text" placeholder="Pick their Game" />
-          </div>
-        </div>
-      </div>
-      <div className='footer'>
-        <p className='p'>
-          Choose two games. We'll combine them to show recommendations you'll love!
-        </p>
-      </div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
